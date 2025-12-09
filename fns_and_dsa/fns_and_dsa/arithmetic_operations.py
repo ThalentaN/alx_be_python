33719b9777b0999ed22207123a1f5497aebf7602
Thalenta Nkala <nkalathalenta@gmail.com>
2025-12-09 21:24:49 +0200

    Refactor perform_operation to accept operation type
    Updated the perform_operation function to include operation parameter and added docstring for clarity.

--- fns_and_dsa/fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/fns_and_dsa/arithmetic_operations.py
@@ -1,5 +1,18 @@
 
-def perform_operation(num1, num2):
+
+def perform_operation(num1: float, num2: float, operation: str):
+    """
+    Perform basic arithmetic operations on two numbers.
+
+    Parameters:
+        num1 (float): The first number.
+        num2 (float): The second number.
+        operation (str): The operation to perform. 
+                         Accepted values: 'add', 'subtract', 'multiply', 'divide'.
+
+    Returns:
+        float or str: The result of the arithmetic operation, or an error message for invalid cases.
+    """
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
